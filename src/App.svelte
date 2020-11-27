838db6e7e21ab6d843fd2bae27975f46b9617a37
soyrbto <soyrbto@gmail.com>
2020-11-27 09:48:11 -0400

    update

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,11 @@
 <script>
   import page from "page";
   import MainPage from "./sections/MainPage.svelte";
+  import Article from "./sections/Article.svelte";
 
   let current = MainPage;
   page("/", () => (current = MainPage));
+  page("/article", () => (current = Article));
   page.start();
 </script>
 
